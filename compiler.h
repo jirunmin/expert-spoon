dd96cb990cd52d3897c77cda082d4faf9b1293b1
jirunmin <jirunmin0101@163.com>
2024-08-07 23:58:33 +0800

    prepare lexer

--- compiler.h
+++ compiler.h
@@ -8,7 +8,13 @@ struct pos
 {
     int line;
     int col;
-    const char* filename;
+    const char *filename;
+};
+
+enum
+{
+    LEXICAL_ANALYSIS_ALL_OK,
+    LEXICAL_ANALYSIS_INPUT_ERROR
 };
 
 enum
@@ -31,22 +37,47 @@ struct token
     union
     {
         char cval;
-        const char* sval;
+        const char *sval;
         unsigned int inum;
         unsigned long lnum;
         unsigned long long llnum;
-        void* any;
+        void *any;
     };
-    
 
     // True if there is whitespace between the token and the next token
     //  i.e * a for operator token * would mean whitespace would be set for token "a"
     bool whitespace;
 
     // (5+10+20)
-    const char* between_brackets;
+    const char *between_brackets;
 };
 
+struct lex_process;
+typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process *process);
+typedef char (*LEX_PROCESS_PEEK_CHAR)(struct lex_process *process);
+typedef void (*LEX_PROCESS_PUSH_CHAR)(struct lex_process *process, char c);
+
+struct lex_process_functions
+{
+    LEX_PROCESS_NEXT_CHAR next_char;
+    LEX_PROCESS_PEEK_CHAR peek_char;
+    LEX_PROCESS_PUSH_CHAR push_char;
+};
+
+struct lex_process
+{
+    struct pos pos;
+    struct vector *token_vec;
+    struct compile_process *compiler;
+
+    int current_expression_count;
+    struct buffer *parentheses_buffer;
+    struct lex_process_functions *function;
+
+    // This will be private data that the lexer does not understand
+    // but the person using the lexer does understand
+    void *private;
+};
 
 enum
 {
@@ -59,6 +90,7 @@ struct compile_process
     // The flags in regards to how this file should be compiled
     int flags;
 
+    struct pos pos;
     struct compile_process_input_file
     {
         FILE *fp;
@@ -71,4 +103,13 @@ struct compile_process
 int compile_file(const char *filename, const char *out_filename, int flags);
 struct compile_process *compile_process_create(const char *filename, const char *filename_out, int flags);
 
+char compile_process_next_char(struct lex_process *lex_process);
+char compile_process_peek_char(struct lex_process *lex_process);
+void compile_process_push_char(struct lex_process *lex_process, char c);
+
+struct lex_process *lex_process_create(struct compile_process *compiler, struct lex_process_functions *functions, void *private);
+void lex_process_free(struct lex_process *process);
+void *lex_process_private(struct lex_process *process);
+struct vector *lex_process_tokens(struct lex_process *process);
+int lex(struct lex_process *process);
 #endif
