44d4ada50cc579ffd682556db2129e638dfae494
jirunmin <jirunmin0101@163.com>
2024-08-07 22:04:51 +0800

    token structures

--- compiler.c
+++ compiler.c
@@ -1,8 +1,8 @@
 #include "compiler.h"
-int compile_file(const char* filename, const char* out_filename, int flags)
+int compile_file(const char *filename, const char *out_filename, int flags)
 {
-    struct compile_process* process = compile_process_create(filename, out_filename, flags);
-    if(!process)
+    struct compile_process *process = compile_process_create(filename, out_filename, flags);
+    if (!process)
         return COMPILER_FAILED_WITH_ERRORS;
 
     // Perform lexical analysis
