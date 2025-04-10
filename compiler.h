008c5bc532f33c88596003f741efe1a6980369ad
jirunmin <jirunmin0101@163.com>
2025-04-11 00:43:31 +0800

    label system

--- compiler.h
+++ compiler.h
@@ -171,6 +171,26 @@ struct symbol
     void *data;
 };
 
+struct codegen_entry_point
+{
+    // ID of the entry point
+    int id;
+};
+
+struct codegen_exit_point
+{
+    // ID of the exit point
+    int id;
+};
+
+struct code_generator
+{
+    // vector of struct codegen_entry_point*
+    struct vector *entry_points;
+    // vector of struct codegen_exit_point*
+    struct vector *exit_points;
+};
+
 struct compile_process
 {
     // The flags in regards to how this file should be compiled
@@ -203,6 +223,9 @@ struct compile_process
         // struct vector *mutiple symbol tables stored in here.
         struct vector *tables;
     } symbols;
+
+    // Pointer to our codegenerator.
+    struct code_generator *generator;
 };
 
 enum
@@ -588,6 +611,7 @@ struct vector *lex_process_tokens(struct lex_process *process);
 int lex(struct lex_process *process);
 int parse(struct compile_process *process);
 int codegen(struct compile_process *process);
+struct code_generator *codegenerator_new(struct compile_process *process);
 
 /**
  * Build tokens for the input string.
--- cprocess.c
+++ cprocess.c
@@ -29,6 +29,7 @@ struct compile_process *compile_process_create(const char *filename, const char 
     process->ofile = out_file;
     process->pos.line = 1;
     process->pos.col = 1;
+    process->generator = codegenerator_new(process);
 
     symresolver_initialize(process);
     symresolver_new_table(process);
